45a7eaf54ccafac2079ad92bc08b6a9be2274cc8
Тимофій Хлопецький <trollface337001@gmail.com>
2025-10-29 23:31:03 +0100

    Add files via upload

---
+++ P3.cpp
@@ -0,0 +1,47 @@
+﻿
+
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		int a = 0, b = 0, c = 0;
+		cout << "GIB DIE ZAHLEN EIN \n";
+		cin >> a >> b;
+		cout << "TIPPE \n 0 BREAK FREE \n 1 ODER \n 2 UND \n 3 XOR \n";
+		cin >> c;
+		if (c == 1)
+		{
+			cout << a << "=" << bitset<4>(a) << "\n";
+			cout << b << "=" << bitset<4>(b) << "\n";
+			cout << "DAS ERGEBNIS IST: " << (a | b) << "\n";
+		}
+		else if (c == 2)
+		{
+			cout << a << "=" << bitset<4>(a) << "\n";
+			cout << b << "=" << bitset<4>(b) << "\n";
+			cout << "DAS ERGEBNIS IST: " << (a & b) << "\n";
+		}
+		else if (c == 3)
+		{
+			cout << a << "=" << bitset<4>(a) << "\n";
+			cout << b << "=" << bitset<4>(b) << "\n";
+			cout << "DAS ERGEBNIS IST: " << (a ^ b) << "\n";
+		}
+		else if (c == 0)
+		{
+			break;
+		}
+		else
+		{
+			cout << "FALSCHE EINGABE \n";
+		}
+	}
+	
+	return 0;
+
+}
+
